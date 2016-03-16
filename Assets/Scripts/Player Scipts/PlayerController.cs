e99faf6cd6d0f7d2d5bb7414ea362cd7424691cb
NateTheGreat12 <couchpotatob@gmail.com>
2016-03-16 14:24:47 -0700

    Added Pistol and Weapon Controller script

--- Assets/Scripts/Player Scipts/PlayerController.cs
+++ Assets/Scripts/Player Scipts/PlayerController.cs
@@ -5,14 +5,15 @@ public class PlayerController : MonoBehaviour {
 	public float moveForce = 360f;
 	public float jumpForce = 50f;
 	public float maxSpeed = 15f;
+	public int airDragMultiplier = 2;
 	public float maxJumpSpeed = 50f;
-	public float yForceAwayFromWall = 300;
+	public float yForceAwayFromWall = 400;
 	public float xForceAwayFromWall = 600;
 
 	[HideInInspector]public bool jump;
-	[HideInInspector]public bool grounded;
+	/** [HideInInspector] **/public bool grounded;
 	[HideInInspector]public int jumpLimit = 2;
-	[HideInInspector]private int jumpNumber = 0;
+	/** [HideInInspector] **/public int jumpNumber = 0;
 
 	private Rigidbody2D rb;
 	private GameObject wallObject;
@@ -21,8 +22,7 @@ public class PlayerController : MonoBehaviour {
 	[HideInInspector]public Transform groundCheck;
 	[HideInInspector]public bool movementControl = true;
 
-	private int ticks
-	;
+	private int ticks;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();	
@@ -30,17 +30,26 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+	// Controlls
 	void Update () {
 		//cast a line to see wether or not it hit the ground
-		grounded = Physics2D.Linecast (transform.position, groundCheck.position);
+		grounded = Physics2D.Linecast (transform.position, groundCheck.position );
 
-		if ( (Input.GetButtonDown ("Jump") ) && (jumpNumber < jumpLimit) ) {
+		// jumping on the ground and off of walls and other objects
+		if ((Input.GetButtonDown ("Jump")) && (jumpNumber < (jumpLimit + 1) && againstWall)) {
 			jump = true;
-			if (!againstWall) {
-				jumpNumber++;
-			}
-			
+			jumpNumber++;
 		}
+		else if( (Input.GetButtonDown ("Jump") )  && (jumpNumber < jumpLimit) ) {
+			jump = true;
+			jumpNumber++;
+		}
+		// go back through platforms
+		if (Input.GetKeyDown (KeyCode.S)) {
+			// playerlayer, groundlayer, ignore collision?
+			Physics2D.IgnoreLayerCollision (9, 13, rb.velocity.y < 0.05);
+		}
+
 		/* if (Input.GetKeyDown(KeyCode.W) ) {
 			rb.gravityScale = -rb.gravityScale;
 		}*/
@@ -48,54 +57,83 @@ public class PlayerController : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
-		if (ticks >= 8) {
+		
+		if (ticks >= 6) {
 			movementControl = true;
 		}
 
 		if (movementControl) {
+			
 			//horizontal movement
 			float moveHorizontal = Input.GetAxis ("Horizontal");
 			Vector2 movement = new Vector2 (moveHorizontal, 0.0f);
+
+			//add some air drag
+			if (!againstWall && !grounded) {
+				movement /= airDragMultiplier;
+			}
+
 			//force upward can be implemented to reverse gravity
-			Vector2 jumpVector = new Vector2 (0.0f, ((jumpForce - rb.velocity.y * 45) * Mathf.Sign (rb.gravityScale)));
+			Vector2 jumpVector = new Vector2 (0.0f, ((jumpForce - rb.velocity.y * 45) * Mathf.Sign (rb.gravityScale) ) );
 
 			//max speeds
 			if (Mathf.Abs (rb.velocity.x) > maxSpeed) {
-				rb.velocity = new Vector2 (Mathf.Sign (rb.velocity.x) * maxSpeed, rb.velocity.y);
+				rb.velocity = new Vector2 (Mathf.Sign (rb.velocity.x) * maxSpeed, rb.velocity.y)  ;
 			}
 
 			if (Mathf.Abs (rb.velocity.y) > maxJumpSpeed) {
 				rb.velocity = new Vector2 (rb.velocity.x, Mathf.Sign (rb.velocity.y * maxJumpSpeed));
 			}
+
 			//reverse direction and disable movement for a moment
 			if (jump && againstWall) {
-				rb.AddForce( new Vector2(-xForceAwayFromWall  * Mathf.Sign(wallObject.transform.position.x - this.transform.position.x) , jumpVector.y + yForceAwayFromWall) );
+				rb.AddForce( new Vector2(-xForceAwayFromWall  * Mathf.Sign(wallObject.transform.position.x - this.transform.position.x) , jumpVector.y + yForceAwayFromWall)  ) ;
 				movementControl = false;
 				jump = false;
 				ticks = 0;
+
 			}else if (jump) {
-				rb.AddForce (jumpVector); 
+				rb.AddForce (jumpVector ); 
 				jump = false;
 
 			}if (movementControl) {
-				rb.AddForce (movement * (moveForce /* * Mathf.Sign(rb.gravityScale)*/));
+				rb.AddForce (movement * (moveForce * Mathf.Sign(rb.gravityScale) )  );
 			}
 		}
 	} 
 		
 	 void OnCollisionStay2D(Collision2D other){
-		if(grounded && rb.velocity.y < 0.09){
+		//if not moving vertically and not against a wall then reset the jump number
+		if ((grounded && rb.velocity.y < 0.05) && (!againstWall)) {
 			jumpNumber = 0;
+
+		// jumping off of other objects( just one for now )
+		} else if (grounded && (!againstWall) && (!other.gameObject.tag.Equals ("Platform") || !other.gameObject.tag.Equals ("Wall") )   ) {
+			jumpNumber--;	
 		}
 		if (other.gameObject.tag.Equals ("Wall")) {
 			againstWall = true;
 			wallObject = other.gameObject;
 		} 
+			
+	}
 
+	void OnCollisionEnter2D(Collision2D other){
+		//Weapon Pickups
+		if(other.gameObject.tag.Equals("Weapon") ){
+			other.transform.SetParent (this.transform);
+			//destroy rigibbody and disable collider components
+			//you can only destroy and recreate rigidbodies :/
+			Destroy( other.gameObject.GetComponent<Rigidbody2D>() );
+			foreach (BoxCollider2D collisionBox in other.gameObject.GetComponents<BoxCollider2D>()) {
+				collisionBox.enabled = false;
+			}
+
+			other.gameObject.GetComponent<WeaponController> ().attachedTo = this.gameObject;
+		}
 	}
 
 	void OnCollisionExit2D(Collision2D other){
 		againstWall = false;
-
 	}
 }
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+	//refrence to the object that this follows
+	[HideInInspector]public GameObject attachedTo;
+	[HideInInspector]public float angle = 0;
+	[HideInInspector]public bool isAi;
+	public float radius = 1;
+	public double threshold = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isAi) {
+			if (attachedTo != null) {
+				float mouseX = Input.mousePosition.x;
+				float mouseY = Input.mousePosition.y;
+				Vector2 mousePosition = Camera.main.ScreenToWorldPoint (new Vector3 (mouseX, mouseY, 0));
+				//angle between attached object and mouse(in radians)
+				angle = Mathf.Atan2 (attachedTo.transform.position.y - mousePosition.y, attachedTo.transform.position.x - mousePosition.x) + 135;
+
+				//polar(angle and radius) --> rectangle(x and y) coordinates MUST BE IN RADIANS
+				// x = rcos(theta)
+				float x = radius * Mathf.Cos (angle);
+				// y = rsin(theta)
+				float y = radius * Mathf.Sin (angle);
+				float angleInDeg = ((angle - 135) * 180 / Mathf.PI) + 185;
+				if (Mathf.Abs (this.transform.rotation.eulerAngles.z - angleInDeg) > threshold) {
+					transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angleInDeg));
+					transform.position = new Vector2 (x + attachedTo.transform.position.x, y + attachedTo.transform.position.y);
+				}
+			}
+		}
+	}
+
+}
