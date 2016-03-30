165466affbe322bc0e39b8a8a31c005bace621fb
NateTheGreat12 <couchpotatob@gmail.com>
2016-03-30 14:04:00 -0700

    added boolets to shoot but no damage

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+	public float speed = 50;
+	private Rigidbody2D rb;
+	[HideInInspector]public GameObject parent; // the gun shooting the object
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		float angle = parent.GetComponent<WeaponController>().angle + 0.25f;
+		rb.velocity = new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle)) * speed;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		Destroy (this.gameObject);
+	}
+}
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+/**
+ * A general class for player controlled weapons 
+ **/
 public class WeaponController : MonoBehaviour {
 	//refrence to the object that this follows
 	[HideInInspector]public GameObject attachedTo;
 	[HideInInspector]public float angle = 0;
-	[HideInInspector]public bool isAi;
+	[HideInInspector]public float angleInDeg = 0;
+	public GameObject bullet;
+	public float fireRate = 30;
 	public float radius = 1;
 	public double threshold = 1;
 
+	private int ticks;
 	// Use this for initialization
 	void Start () {
-		
+		ticks = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!isAi) {
 			if (attachedTo != null) {
 				float mouseX = Input.mousePosition.x;
 				float mouseY = Input.mousePosition.y;
@@ -29,13 +33,29 @@ public class WeaponController : MonoBehaviour {
 				float x = radius * Mathf.Cos (angle);
 				// y = rsin(theta)
 				float y = radius * Mathf.Sin (angle);
-				float angleInDeg = ((angle - 135) * 180 / Mathf.PI) + 185;
+				angleInDeg = ((angle - 135) * 180 / Mathf.PI) + 185;
 				if (Mathf.Abs (this.transform.rotation.eulerAngles.z - angleInDeg) > threshold) {
+				
+					//flip weapon
+				if ( (angleInDeg > 90) && (angleInDeg < 270) ) {
+					GetComponent<SpriteRenderer> ().flipY = true;
+				}else { GetComponent<SpriteRenderer> ().flipY = false; }
+					
 					transform.rotation = Quaternion.Euler (new Vector3 (0, 0, angleInDeg));
+			
 					transform.position = new Vector2 (x + attachedTo.transform.position.x, y + attachedTo.transform.position.y);
+
+				}
+			if (Input.GetMouseButton (0)) {
+				Debug.Log("Pressed left click.");
+				if (ticks >= fireRate) {
+					bullet.GetComponent<BulletController> ().parent = this.gameObject;
+					Instantiate (bullet, this.transform.position, this.transform.rotation);
+					ticks = 0;
 				}
 			}
 		}
+		ticks++;
 	}
 
 }
