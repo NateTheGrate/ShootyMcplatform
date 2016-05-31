5bc8347abc72190392c1c666a01e55920ccafd1c
NateTheOriginalG <couchpotatob@gmail.com>
2016-05-31 10:29:38 -0700

    Main Menu works now but needs "nope" sound

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainMenuController : MonoBehaviour {
+	public GameObject singlePlayerButtonText;
+	public GameObject multiplayerButtonText;
+	public GameObject optionsButtonText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void startSinglePlayer(){
+		SceneManager.LoadScene ("test_flat");
+	}
+
+	public void startMultiplayer(){
+		multiplayerButtonText.GetComponent<Text>().text = "Under Construction";
+	}
+
+	public void startOptions(){
+		optionsButtonText.GetComponent<Text>().text = "Under Construction";
+	}
+}
